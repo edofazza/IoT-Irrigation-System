e66c3778da69d7803b8755cf866bf1e7020a0f86
edofazza <fazzari.edoardo@gmail.com>
2021-08-07 13:08:15 +0200

    stuff

--- irrigation_system/src/main/java/it/unipi/iot/irrigationsystem/Collector.java
+++ irrigation_system/src/main/java/it/unipi/iot/irrigationsystem/Collector.java
@@ -1,4 +1,45 @@
 package it.unipi.iot.irrigationsystem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Collector {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String command = "";
+        String[] chunks;
+
+        printCommands();
+
+        while(true) {
+            System.out.print(">!");
+            try {
+                command = br.readLine();
+                chunks = command.split(" ");
+
+                if(chunks[0].equals("quit"))
+                    break;
+
+                else if(chunks[0].equals("help"))
+                    printCommands();
+
+                else if(chunks[0].equals("getSensorsList"))
+                    System.out.println("PASS"); //TODO
+
+                else
+                    System.out.println("Invalid command");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void printCommands() {
+        System.out.println("Commands list:" +
+                "\n\t!getSensorsList: show list of all available sensors" +
+                "\n\t!help: print commands list" +
+                "\n\t!quit: quit the program");
+    }
 }
