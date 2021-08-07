7908a1d945404797ceb9100541477a9086183066
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-08-07 16:20:58 +0200

    changed names

--- coap_network/temperature/resources/temperature_sensor.c
+++ coap_network/temperature/resources/temperature_sensor.c
@@ -4,23 +4,23 @@
 #include "dev/leds.h"
 #include "sys/log.h"
 
-static void get_presence_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
-static void presence_event_handler(void);
+static void get_temperature_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset);
+static void temperature_event_handler(void);
 
 EVENT_RESOURCE(temperature_sensor,
                "title=\"Temperature sensor\";obs",
-               get_presence_handler,
+               get_temperature_handler,
                NULL,
                NULL,
                NULL,
-               presence_event_handler);
+               temperature_event_handler);
 
-static void get_presence_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
+static void get_temperature_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
     
 }
 
-static void presence_event_handler(void)
+static void temperature_event_handler(void)
 {
     
 }
