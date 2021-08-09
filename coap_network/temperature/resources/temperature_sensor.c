8aabdb87c837edcf5d187aae089ef226e6dbeba7
edofazza <fazzari.edoardo@gmail.com>
2021-08-09 19:17:39 +0200

    End temperature doc + code

--- coap_network/temperature/resources/temperature_sensor.c
+++ coap_network/temperature/resources/temperature_sensor.c
@@ -24,23 +24,26 @@ EVENT_RESOURCE(temperature_sensor,
 static void get_temperature_handler(coap_message_t *request, coap_message_t *response, uint8_t *buffer, uint16_t preferred_size, int32_t *offset)
 {
     LOG_INFO("Handling temperature get request...\n");
-   
-    static const size_t max_char_len = 4; //-dd\0
     char* msg;
     
     // IF TOO HOT OR TOO COLD SEND A WARNING
     if (temperature < LOWER_BOUND_TEMP)
     {
         LOG_INFO("Temperature lower than normal\n");
-        msg = "WARN hot"; // TODO aggiungere temperatura
+        static const int length = snprintf(NULL, 0,"%d", temperature) + sizeof("WARN hot") + 1;
+        msg = new char[length];
+        snprintf(msg, length, "WARN hot %d", temperature);
     }
     else if (temperature > UPPER_BOUND_TEMP)
     {
         LOG_INFO("Temperature greater than normal\n");
-        msg = "WARN cold";
+        static const int length = snprintf(NULL, 0,"%d", temperature) + sizeof("WARN cold") + 1;
+        msg = new char[length];
+        snprintf(msg, length, "WARN cold %d", temperature);
     }
     else
     {
+        static const size_t max_char_len = 4; //-dd\0
         msg = new char[max_char_len];
         snprintf(msg, max_char_len, "%d", temperature);
     }
@@ -59,10 +62,33 @@ static void put_temperature_handler(coap_message_t *request, coap_message_t *res
 {
     LOG_INFO("Handling temperature put request...\n");
     
-    // TODO: DECIDERE IL FORMATO DELLA RICHIESTA
-    // AGGIORNARE:
-        //LOWER_BOUND_TEMP = new_lower_bound_value;
-        //UPPER_BOUND_TEMP = new_upper_bound_value;
+    size_t len = 0;
+    const uint8_t* payload = NULL;
+    bool success = true;
+    
+    if((len = coap_get_payload(request, &payload)))
+    {
+        char* chunks = strtok((char*)payload, " ");
+        
+        int new_value = atoi(chunks[1]);
+        if (strncmp(chunks[0], "u", strlen("u")))
+        {
+            if (new_value < LOWER_BOUND_TEMP)
+                success = false;
+            else
+                UPPER_BOUND_TEMP = atoi(chunks[1]);
+        }
+        else // update the lower bound
+        {
+            if (new_value > UPPER_BOUND_TEMP)
+                success = false;
+            else
+                LOWER_BOUND_TEMP = atoi(chunks[1]);
+        }
+    }
+
+    if(!success)
+        coap_set_status_code(response, BAD_REQUEST_4_00);
 }
 
 static void temperature_event_handler(void)
