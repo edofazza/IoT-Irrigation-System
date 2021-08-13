206d6200ec26aa05bc619f008bc68fcd37bbd568
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-08-13 10:09:03 +0200

    Thread safe code

--- irrigation_system/src/main/java/it/unipi/iot/irrigationsystem/coap/rain/RainSensor.java
+++ irrigation_system/src/main/java/it/unipi/iot/irrigationsystem/coap/rain/RainSensor.java
@@ -1,6 +1,10 @@
 package it.unipi.iot.irrigationsystem.coap.rain;
 
+import it.unipi.iot.irrigationsystem.coap.soilmoisture.SoilMoistureNetwork;
+import it.unipi.iot.irrigationsystem.coap.tap.TapActuator;
+import it.unipi.iot.irrigationsystem.coap.temperature.TemperatureSensorNetwork;
 import it.unipi.iot.irrigationsystem.database.IrrigationSystemDbManager;
+import it.unipi.iot.irrigationsystem.enumerate.SwitchStatus;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapHandler;
 import org.eclipse.californium.core.CoapObserveRelation;
@@ -11,7 +15,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class RainSensor {
     private CoapClient clientRainSensor;
     private CoapObserveRelation observeRain;
-    private AtomicBoolean isRaining=new AtomicBoolean();
+    private AtomicBoolean isRaining;
+
+    private SoilMoistureNetwork moistureNetwork;
+    private TapActuator tapActuator;
+    private TemperatureSensorNetwork temperatureSensorNetwork;
 
     public void addRainSensor(String ip) {
         System.out.println("The rain sensor: [" + ip + "] + is now registered");
@@ -24,6 +32,13 @@ public class RainSensor {
                         String responseString = response.getResponseText();
 
                         isRaining.set(responseString.equals("raining"));
+
+                        if (moistureNetwork != null)
+                            moistureNetwork.turnSwitch(isRaining.get() ? SwitchStatus.ON : SwitchStatus.OFF);
+                        if (temperatureSensorNetwork != null)
+                            temperatureSensorNetwork.turnSwitch(isRaining.get() ? SwitchStatus.ON : SwitchStatus.OFF);
+                        if (tapActuator != null)
+                            tapActuator.turnSwitch(isRaining.get() ? SwitchStatus.ON : SwitchStatus.OFF);
 
                         IrrigationSystemDbManager.insertRainStatus(isRaining.get());
                     }
@@ -49,5 +64,13 @@ public class RainSensor {
     public void printDevice() {
         if (clientRainSensor != null)
             System.out.println("Rain Sensor:\n\t" + clientRainSensor.getURI() + "\n");
+    }
+
+    public void addControlledDevices(SoilMoistureNetwork moistureNetwork,
+                                     TapActuator tapActuator,
+                                     TemperatureSensorNetwork temperatureSensorNetwork) {
+        this.moistureNetwork = moistureNetwork;
+        this.tapActuator = tapActuator;
+        this.temperatureSensorNetwork = temperatureSensorNetwork;
     }
 }
