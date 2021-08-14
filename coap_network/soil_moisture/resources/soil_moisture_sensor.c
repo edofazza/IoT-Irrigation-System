078ce93958c6af2a5613b53ed593a50fd8820fcb
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-08-14 09:41:45 +0200

    error correction

--- coap_network/soil_moisture/resources/soil_moisture_sensor.c
+++ coap_network/soil_moisture/resources/soil_moisture_sensor.c
@@ -15,10 +15,10 @@
 
 #define VARIATION 0.02
 
-static double LOWER_BOUND_SOIL_TENSION = -0.6; // bar
-static double UPPER_BOUND_SOIL_TENSION = -0.4; // bar
+static double LOWER_BOUND_SOIL_TENSION = -0.60; // bar
+static double UPPER_BOUND_SOIL_TENSION = -0.40; // bar
 
-static double soilTension = -0.5;
+static double soilTension = -0.50;
 
 
 
