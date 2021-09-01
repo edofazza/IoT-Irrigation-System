18d553f1ec3fda9bfdfd86302c8b3eeca666b9a0
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-09-01 11:47:23 +0200

    hopeless try 17: try to elide printf

--- mqtt_network/reservoir copia/reservoir_level_device.c
+++ mqtt_network/reservoir copia/reservoir_level_device.c
@@ -253,7 +253,7 @@ PROCESS_THREAD(humidity_analyzer_process, ev, data)
                 available = level*WIDTH*DEPTH;
                 sprintf(app_buffer, "{\"node\": %d, \"reservoir_availability\": %i, \"unit\": \"cm^3\"}", node_id, available);
                 mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer, strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
-                LOG_INFO("Sensed water level is: %i cm, reservoir water availability is %i cm^3\n", level, available);
+                //LOG_INFO("Sensed water level is: %i cm, reservoir water availability is %i cm^3\n", level, available);
                 STATE_MACHINE_PERIODIC = PUBLISH_INTERVAL;
 			}
 			else if ( state == STATE_DISCONNECTED )
