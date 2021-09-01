00445d1555491cd624d385f28ecc55cd0c26b53a
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-09-01 16:14:55 +0200

    copy in the original files

--- mqtt_network/reservoir/reservoir_sensor.c
+++ mqtt_network/reservoir/reservoir_sensor.c
@@ -3,25 +3,26 @@
 /*   The following code is just a simulation of the output of a level sensor and the corresponding actuator that
      puts or fetches the water from the reservoir*/
 
-static int sensed_level=MAX_LEVEL;
-static int capacity = MAX_LEVEL*WIDTH*DEPTH;
-
 static int simulate_level(){
     return sensed_level;
+}
+
+static int get_capacity(){
+    return capacity;
 }
 
 static void put_get_water(int quantity){
 //assuming rectangular reservoir
     capacity += quantity;
     sensed_level = capacity/WIDTH/DEPTH;
-    if (sensed_level>MAX_LEVEL){
-        sensed_level = MAX_LEVEL;
+    if (sensed_level>RES_MAX_LEVEL){
+        sensed_level = RES_MAX_LEVEL;
     }
     else if (sensed_level<0){
         sensed_level = 0;
     }
-    if (capacity>MAX_LEVEL*WIDTH*DEPTH){
-        capacity = MAX_LEVEL*WIDTH*DEPTH;
+    if (capacity>RES_MAX_LEVEL*WIDTH*DEPTH){
+        capacity = RES_MAX_LEVEL*WIDTH*DEPTH;
     }
     else if (capacity<0){
         capacity = 0;
