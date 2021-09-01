a764005635a4097e8b7b0a25f4e36917fef4c560
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-09-01 11:36:37 +0200

    hopeless try 14: added print

--- mqtt_network/reservoir copia/reservoir_level_device.c
+++ mqtt_network/reservoir copia/reservoir_level_device.c
@@ -253,6 +253,7 @@ PROCESS_THREAD(humidity_analyzer_process, ev, data)
                 available = level*WIDTH*DEPTH;
                 sprintf(app_buffer, "{\"node\": %d, \"reservoir_availability\": %d, \"unit\": \"cm^3\"}", node_id, available);
                 mqtt_publish(&conn, NULL, pub_topic, (uint8_t *)app_buffer, strlen(app_buffer), MQTT_QOS_LEVEL_0, MQTT_RETAIN_OFF);
+                printf("Sensed water level is: %d cm, reservoir water availability is %d cm^3\n", level, available);
 			}
 			else if ( state == STATE_DISCONNECTED )
 			{
