f16b9dc154fc9b1ff77d2e5ffa246b004db61ac8
Mirco-Ramo <m.ramo@studenti.unipi.it>
2021-09-01 10:32:20 +0200

    hopeless try 3

--- mqtt_network/reservoir copia/reservoir_parameters.h
+++ mqtt_network/reservoir copia/reservoir_parameters.h
@@ -1,7 +1,7 @@
 //assuming rectangular reservoirs of capacity 1000 litres
 //#define RES_WIDTH 200             //2m = 200cm
 //#define RES_DEPTH 100               //1m = 100cm
-//#define RES_MAX_LEVEL 50           //50 cm
+#define MAX_LEVEL 50           //50 cm
 #define WATER_SPEED 0.0005    /* 0.0005cm/s   https://www.arpa.vda.it/it/acqua/acque-sotterranee/cosa-sono-le-acque-sotterranee */
 #define SECTION 200             //2m
 //Total capacity is 200*100*50 = 1e6 cm^3 = 1000 l
